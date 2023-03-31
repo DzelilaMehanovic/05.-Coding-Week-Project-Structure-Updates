a8fd617a3a339ff38d4f4df0e1904e803e4a8555
DzelilaMehanovic <dzelila.mehanovic@stu.ibu.edu.ba>
2023-03-31 13:16:21 +0200

    Add files via upload

---
+++ rest/services/StudentService.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'BaseService.php';
+require_once __DIR__."/../dao/StudentsDao.class.php";
+
+class StudentService extends BaseService{
+    public function __construct(){
+        parent::__construct(new StudentsDao);
+    } 
+
+    public function add($entity){
+        return parent::add($entity);
+        /*
+        example 
+        send an email
+        if(!validateField($entity['first_name'])){
+            //error
+        }
+        */
+    }
+}
+?>
